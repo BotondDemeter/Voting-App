e83bfeb941cbc98dfc906c604cd027817e78e5db
BotondDemeter <demeter.botond@student.ms.sapientia.ro>
2024-10-17 15:38:34 +0300

    front-end first commit

---
+++ front-end/voting-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to my voting app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
